8190ce2e86717763396a30b99313d9bc527db7bb
cytsaibe <jiyoutsai@gmail.com>
2025-05-04 03:24:05 +0000

    l7

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,4 +1,4 @@
-def get_status(bmi:float):
+def get_status(bmi:float)->str:
     if bmi < 18.5:
         status = "體重過輕"
     elif bmi < 24:
@@ -11,9 +11,8 @@ def get_status(bmi:float):
         status = "中度肥胖"
     else:
         status = "重度肥胖"
-    print(f"狀態:{status}")
 
-    pass
+    return status
 
 def main():
     height:int = int(input("請輸入身高公分"))
@@ -23,7 +22,8 @@ def main():
     print(f"身高:{height}""cm")
     print(f"體重:{weight}""kg")
     print(f"BMI:{BMI}")
-    get_status(BMI)
+    status:str = get_status(BMI)
+    print(f"狀態為:{status}")
     
 if __name__ =='__main__':
     main()
